0ca35b2b3656d1454c596426aba2a5b036b56c71
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2026-01-16 00:19:38 +0530

    Add initial implementation of simple calculator with operation options

--- 01_PYTHON_BASICS/project_01_number_guessing/main.py
+++ 01_PYTHON_BASICS/project_01_number_guessing/main.py
@@ -1,9 +1,9 @@
 import random
 
 secret_number = random.randint(1, 10)
- # For testing purposes; remove or comment out in production
-guess = 0
-attempts = 0
+
+guess = 0 
+attempts = 0 
 
 while guess != secret_number:
  guess = int(input("Guess a number between 1 and 10: "))
