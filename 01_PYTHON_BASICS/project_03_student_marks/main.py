413815549efa1328fcdfe9d5eced9967791b6ccf
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2026-01-16 12:46:03 +0530

    Add initial implementation of student marks manager with menu options

---
+++ 01_PYTHON_BASICS/project_03_student_marks/main.py
@@ -0,0 +1,9 @@
+students = {}
+
+print("Student Marks Manager")
+print("---------------------")
+print("1. Add student mark")
+print("2. Display all students")
+print("3. Calculate average")
+print("4. Show highest mark")
+print("5. Exit")
