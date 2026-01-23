c631f4c183f4cb17e3be9dd28144d4631102effe
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2026-01-24 00:28:10 +0530

    Add initial implementation of To-Do List Manager

---
+++ 02_file_handling_and_logic/project_04_todo_list/main.py
@@ -0,0 +1,10 @@
+tasks = []
+
+while True:
+    print("\nTo-Do List Manager")
+    print("------------------")
+    print("1. Add task")
+    print("2. View tasks")
+    print("3. Exit")
+
+    choice = input("Enter your choice (1-3): ")
